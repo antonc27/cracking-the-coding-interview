87e1617a6c5f90d42db7762e44d27f2c27e43939
antonc27 sw <antonc27@mail.ru>
2020-04-02 00:26:08 +0200

    _01_01_IsUnique and _01_02_CheckPermutation (with my implementation of QuickSort)

--- src/main/java/arraystring/_01_01_IsUnique.java
+++ src/main/java/arraystring/_01_01_IsUnique.java
@@ -1,13 +1,49 @@
 package arraystring;
 
+import java.util.Arrays;
+
 /**
  * Implement an algorithm to determine if a string has all unique characters.
  */
 
 class _01_01_IsUnique {
 
+    public static void mySortRec(char[] a, int start, int end) {
+        if (end - start < 2) return;
+        char pivot = a[start];
+        int pivotingIdx = start + 1;
+        for (int i = start + 1; i < end; i++) {
+            char nextChar = a[i];
+            if (nextChar <= pivot) {
+                a[i] = a[pivotingIdx];
+                a[pivotingIdx] = nextChar;
+                pivotingIdx++;
+            }
+        }
+        pivotingIdx--;
+        a[start] = a[pivotingIdx];
+        a[pivotingIdx] = pivot;
+
+        mySortRec(a, start, pivotingIdx);
+        mySortRec(a, pivotingIdx + 1, end);
+    }
+
+    public static void mySort(char[] a) {
+        mySortRec(a, 0, a.length);
+    }
+
     boolean isUnique(String str) {
-        throw new UnsupportedOperationException();
+        if (str.isEmpty()) return true;
+        char[] chars = str.toCharArray();
+        //Arrays.sort(chars);
+        mySort(chars);
+        char prevChar = chars[0];
+        for (int i = 1; i < chars.length; i++) {
+            char currentChar = chars[i];
+            if (currentChar == prevChar) return false;
+            prevChar = currentChar;
+        }
+        return true;
     }
 
 }
--- src/main/java/arraystring/_01_02_CheckPermutation.java
+++ src/main/java/arraystring/_01_02_CheckPermutation.java
@@ -5,6 +5,15 @@ package arraystring;
  */
 class _01_02_CheckPermutation {
     boolean check(String a, String b) {
-        throw new UnsupportedOperationException();
+        if (a.isEmpty() && b.isEmpty()) return true;
+        if (a.length() != b.length()) return false;
+        char[] aa = a.toCharArray();
+        _01_01_IsUnique.mySort(aa);
+        char[] bb = b.toCharArray();
+        _01_01_IsUnique.mySort(bb);
+        for (int i = 0; i < aa.length; i++) {
+            if (aa[i] != bb[i]) return false;
+        }
+        return true;
     }
 }
